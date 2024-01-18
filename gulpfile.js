16bd66d090763f4df2708695a93b6722600e5784
James Murgatroyd <james@jamesmurgatroyd.com>
2024-01-18 14:41:40 -0700

    tweaks
    Signed-off-by: James Murgatroyd <james@jamesmurgatroyd.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,168 +1,139 @@
-'use strict';
+// Load Gulp...of course
+const { src, dest, task, watch, series, parallel } = require('gulp');
 
-// DEPENDENCIES =======================================================
-var gulp        = require('gulp');
-var babel 		= require('gulp-babel');
-var browserSync = require('browser-sync').create();
-var $           = require('gulp-load-plugins')();
-var debug       = require('gulp-debug');
-var del         = require('del');
-var minifyCSS   = require('gulp-minify-css');
-var size        = require('gulp-filesize');
-var reload      = browserSync.reload;
-var argv        = require('yargs').argv;
-var autoprefixer = require('gulp-autoprefixer');
-var uglify      = require('gulp-uglify');
-var concat = require('gulp-concat');
-var gutil = require('gulp-util');
-var gulpif = require('gulp-if');
-var sourcemaps = require('gulp-sourcemaps');
-var notify = require("gulp-notify");
+// CSS related plugins
+var sass         = require( 'gulp-sass' )(require('node-sass'));
+var autoprefixer = require( 'gulp-autoprefixer' );
 
-// FILE PATHS =========================================================
+// JS related plugins
+var uglify       = require( 'gulp-uglify' );
+var babelify     = require( 'babelify' );
+var browserify   = require( 'browserify' );
+var source       = require( 'vinyl-source-stream' );
+var buffer       = require( 'vinyl-buffer' );
+var stripDebug   = require( 'gulp-strip-debug' );
 
-var source = {
-  vendor  : 'source/vendor',
-  style   : 'source/styles/',
-  styles  : 'source/styles/**/**/*.scss',
-  scripts : 'source/scripts/*.js',
-  images  : 'source/images/*.{png,jpg,gif}',
-  sprite  : 'source/images/*.png',
-  svgs    : 'source/images/*.svg'
+// Utility plugins
+var rename       = require( 'gulp-rename' );
+var sourcemaps   = require( 'gulp-sourcemaps' );
+var notify       = require( 'gulp-notify' );
+var plumber      = require( 'gulp-plumber' );
+var options      = require( 'gulp-options' );
+var gulpif       = require( 'gulp-if' );
+var del			 = require( 'del' );
+
+var filter = require( 'gulp-filter' ); // Enables you to work on a subset of the original files by filtering them using a glob.
+var mmq = require( 'gulp-merge-media-queries' ); // Combine matching media queries into one.
+
+
+// Project related variables
+var styleSRC     = './src/scss/site.scss';
+var styleURL     = './assets/css/';
+var mapURL       = './';
+
+var frameworkSRC = './src/scss/framework.scss';
+var adminFrameworkSRC = './src/scss/admin-framework.scss';
+var adminSRC	 = './src/scss/admin.scss';
+
+var templatesSRC = './src/scss/pages/templates/*.scss';
+var templatesURL = './css/templates/';
+var cleanCssGlob = './css/**/*';
+
+var jsSRC        = './src/js/';
+var jsFront      = 'site.js';
+var jsFiles      = [ jsFront ];
+var jsURL        = './assets/js/';
+var cleanJsGlob  = './js/**/*';
+
+var styleWatch   = './src/scss/**/*.scss';
+var jsWatch      = './src/js/**/*.js';
+
+
+
+function globalCss(done) {
+	src( [ styleSRC, adminSRC, frameworkSRC, adminFrameworkSRC ] )
+		.pipe( plumber() )
+		.pipe( sourcemaps.init() )
+		.pipe( sass({
+			errLogToConsole: true,
+			outputStyle: 'nested'
+		}) )
+		.on( 'error', console.error.bind( console ) )
+		.pipe( autoprefixer({ browsers: [ 'last 2 versions', '> 5%', 'Firefox ESR' ] }) )
+		.pipe( mmq({ log: true }) ) 
+		.pipe( sass({
+			errLogToConsole: true,
+			outputStyle: 'compressed'
+		}) )
+		.pipe( rename( { suffix: '.min' } ) )	
+		.pipe( sourcemaps.write( mapURL ) )
+		.pipe( dest( styleURL ) )
+	done();
 };
 
-var assets = {
-  theme   : '',
-  styles  : 'assets/css',
-  scripts : 'assets/js',
-  images  : 'assets/img'
+function cleanStylesScripts(done){
+	return del([ cleanCssGlob, cleanJsGlob ]);
+}
+
+function templatesCss(done){
+
+	src( [ templatesSRC ] )
+		.pipe( plumber() )
+		.pipe( sourcemaps.init() )
+		.pipe( sass({
+			errLogToConsole: true,
+			outputStyle: 'nested'
+		}) )
+		.on( 'error', console.error.bind( console ) )
+		.pipe( autoprefixer({ browsers: [ 'last 2 versions', '> 5%', 'Firefox ESR' ] }) )
+		// .pipe( filter( '**/*.css' ) ) // Filtering stream to only css files.
+		.pipe( mmq({ log: true }) ) // Merge Media Queries only for .min.css version.
+		.pipe( sass({
+			errLogToConsole: true,
+			outputStyle: 'compressed'
+		}) )
+		.pipe( rename( { suffix: '.min' } ) )	
+		.pipe( sourcemaps.write( mapURL ) )
+		.pipe( dest( templatesURL ) )
+	done();
+}
+
+
+
+function js(done) {
+	jsFiles.map( function( entry ) {
+		return browserify({
+			entries: [jsSRC + entry]
+		})
+		.transform( babelify, { presets: [ '@babel/preset-env' ] } )
+		.bundle()
+		// .pipe( plumber() )
+		.pipe( source( entry ) )
+		.pipe( rename( {
+			extname: '.min.js'
+        } ) )
+		.pipe( buffer() )
+		.pipe( gulpif( options.has( 'production' ), stripDebug() ) )
+		.pipe( sourcemaps.init({ loadMaps: true }) )
+		.pipe( uglify() )
+		.pipe( sourcemaps.write( '.' ) )
+		.pipe( dest( jsURL ) )
+	});
+	done();
 };
 
 
-// Browser Sync Config =========================================================
-var bsconfig = {
-    // Customize the BrowserSync console logging prefix
-    logPrefix: '⎋',
-    proxy:  "http://192.168.67.19",
-    //logFileChanges: true,
-    open: "local", //"local", "external", "ui", "ui-external" or "tunnel"
-    notify: true,
-    injectChanges: true
-  }
 
-// AUTOPREFIXER CONFIG ================================================
-var AUTOPREFIXER_BROWSERS = [
-  'ie >= 8',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
-];
- 
+function watch_files() {
+	watch(styleWatch, series(globalCss, templatesCss));
+	watch(jsWatch, series(js));
+	src(jsURL + 'site.min.js')
+	.pipe( notify({ message: 'Gulp is Watching, Happy Coding!' }) )
+}
 
-// COMPILE STYLESHEETS ================================================
-gulp.task('styles', function () {
-  return gulp.src(source.style + '/styles.scss')
-    .pipe($.changed('styles', {
-      extension: '.scss'
-    }))
-    .pipe(sourcemaps.init())
-    .pipe($.sass({
-      precision: 5,
-      outputStyle: 'expanded',
-      onError: function(err) {
-           console.log(assets.theme);
-          return notify().write(err);
-        },
-      stopOnError: false,
-      sourcemap: true,
-      verbose: true,
-      emitCompileError: true,
-      lineNumbers: true,
-      includePaths: [
-        source.styles,
-        //source.vendor + '/bootstrap-sass-official/assets/stylesheets',
-      ]
-    }))
-    .on('error', $.sass.logError)
-    .pipe($.autoprefixer({
-      browsers: AUTOPREFIXER_BROWSERS
-    }))
-    .pipe(sourcemaps.write('maps', {
-      includeContent: false,
-      sourceRoot: 'source'
-    }))
-    //.pipe( concat('brandt-elementor-app.css') )
-    .pipe(gulp.dest(assets.styles))
-    .pipe($.rename({ suffix: '.min' }))
-  	.pipe(minifyCSS({keepBreaks:true}))
-  	.pipe(gulp.dest(assets.styles))
-    .pipe($.size({title: 'styles'}))
-    .pipe($.notify({ message: 'Styles task complete' }))
-    .pipe(browserSync.stream());
-
-});
-
-// Vendor Plugin Scripts ================================================
-gulp.task('plugins', function() {
-  return gulp.src([
-    source.vendor + '/jquery-ui-1.12.1.custom/jquery-ui.min.js',
-    source.vendor + '/js-cookie/src/js.cookie.js',
-    source.vendor + '/OwlCarousel2-2.2.1/dist/owl.carousel.js',
-    source.vendor + '/imagesloaded/imagesloaded.pkgd.min.js',
-    source.vendor + '/jQuery.mmenu-master/dist/jquery.mmenu.all.min.js',
-    source.vendor + '/fastclick/lib/fastclick.js',
-    source.vendor + '/scrollreveal-master/dist/scrollreveal.min.js',
-    source.vendor + '/typeahead.js/dist/typeahead.bundle.js',
-    source.scripts + '/plugins.js',
-  ])
-  //.pipe($.concat('plugins.js'))
-  //.pipe(gulp.dest(assets.scripts))
-  .pipe($.concat('plugins.js'))
-  .pipe(gulp.dest(assets.scripts))
-  .pipe($.rename({ suffix: '.min' }))
-  .pipe($.uglify())
-  //.pipe($.concat('plugins.min.js'))
-  .pipe(sourcemaps.write('maps/'))
-  .pipe(gulp.dest(assets.scripts))
-  .pipe($.notify({ message: 'Plugins task complete' }));
-
-});
-
-// LINT & CONCATENATE JS ==============================================
-gulp.task('js', function () {
-  return gulp.src(source.scripts)
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-        presets: ['@babel/env']
-    }))
-    .pipe( $.jshint() )
-    .pipe( $.jshint.reporter('jshint-stylish') )
-    .pipe( concat('brandt-elementor-app.js') )
-    .pipe( gulp.dest(assets.scripts))
-    .pipe($.rename({ suffix: '.min' }))
-    .pipe($.uglify())
-    .pipe(sourcemaps.write('maps/'))
-    .pipe(gulp.dest(assets.scripts))
-    .pipe($.notify({ message: 'JS task complete' }));
-});
-
-// WATCH FOR CHANGES AND RELOAD =======================================
-gulp.task('serve', ['styles'], function () {
-
-  //browserSync.init(bsconfig);
-  gulp.watch(['**/**/*.php']).on("change", reload);
-  gulp.watch([source.styles], ['styles']);
-  gulp.watch([source.images], ['images']).on("change", reload);
-  gulp.watch([source.scripts], ['js']).on("change", reload);
-});
-
-// Default Task =======================================
-gulp.task('default',  function() {
-    gulp.start('serve','styles', 'plugins', 'js');
-});
-
+task("default", parallel(globalCss, templatesCss, js));
+task("rebuild", series( cleanStylesScripts, globalCss, templatesCss, js ));
+task("clean", cleanStylesScripts);
+task("css", parallel(globalCss, templatesCss));
+task("js", js);
+task("watch", watch_files);
--- inc/layouts/holler-team.php
+++ inc/layouts/holler-team.php
@@ -16,7 +16,7 @@ function _holler_team_template( $settings) {
 	 ob_start();
 ?>
 <article class="holler-widget <?php echo  $style; ?>">
-	 <a href="myModal_<?php echo $rand; ?>"> 	  
+	 <a href="javascript:void(0)" data-modal="<?php echo $rand; ?>" id="myBtn_<?php echo $rand; ?>"> 	  
   	<figure class="img-wrap">
 	     <img src="<?php echo $imgSrc; ?>"  alt='$team_name'/>
 		</figure>
@@ -26,9 +26,9 @@ function _holler_team_template( $settings) {
 	  </header>
 	  </a>  
 </article>
-<div id="myModal_<?php echo $rand; ?>" class="modal">
+<div id="myModal_<?php echo $rand; ?>" class="modal" data-id="<?php echo $rand; ?>">
 	<div class="holler-team-lightbox-wrap">
-	<span class="close">&times;</span>
+	<span class="close close_<?php echo $rand; ?>">&times;</span>
 		<div class="team-lightbox-header">
 		<figure class="img-wrap">
 			<img src="<?php echo $imgSrc; ?>"  alt='$team_name'/>
@@ -43,6 +43,35 @@ function _holler_team_template( $settings) {
 		</div>
 	</div>
 </div>
+
+<script>
+	// Get the modal
+var modal = document.getElementById("myModal_<?php echo $rand; ?>");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn_<?php echo $rand; ?>");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close_<?php echo $rand; ?>")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+</script>
 <?php
   return ob_get_clean();
 }
